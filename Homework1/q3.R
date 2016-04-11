1952a7bae4cc13027abe41597b8ad99355190c3d
Stonre <leiding007@gmail.com>
2016-04-11 04:07:36 -0700

    Update q3.R

--- Homework1/q3.R
+++ Homework1/q3.R
@@ -1,10 +1,14 @@
 library("igraph")
 
 #a)
+cat("Create a random graph by simulating its evolution...")
 g_r <- aging.prefatt.game(1000,pa.exp = 1, aging.exp = -1, aging.bin = 1000, directed = FALSE)
 g_r_d <- degree_distribution(g_r)
+cat("Plot degree distribution...")
 plot(1:length(g_r_d), g_r_d, "h", main="Random Graph Degree Distribution", xlab = "Degree", ylab = "Probability of Degree")
 
 #b)
+cat("Fast greedy method to find the community structure...")
 fc <- fastgreedy.community(g_r)
+cat("Modularity= ")
 modularity(g_r, membership(fc))
