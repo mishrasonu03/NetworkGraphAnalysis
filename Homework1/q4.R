1fb36d5167fb02b5c2d4a6419c24f04e14390eef
Yi Ding <lavenderdingyi@gmail.com>
2016-04-14 16:14:48 -0700

    modified version

--- Homework1/q4.R
+++ Homework1/q4.R
@@ -1,27 +1,33 @@
 library ("igraph")
 
-#part a
-numNodes <- 1000
+mod <- vector(mode="integer", length=10)
 
-g <- forest.fire.game (numNodes, fw.prob=0.37, bw.factor=0.32/0.37, ambs=1, TRUE)
+for(i in 1 : 10){
+  #part a
+  numNodes <- 1000
+  
+  g <- forest.fire.game (numNodes, fw.prob=0.37, bw.factor=0.32/0.37, ambs=1, TRUE)
+  
+  g_in <- degree_distribution(g,mode="in")
+  g_out <- degree_distribution(g,mode="out")
+  
+  hist_in <- hist(degree(g,mode="in"), breaks=seq(-0.5, by=1 , length.out=max(degree(g,mode="in"))+2), main="In Degree Distribution",freq=FALSE,xlab = "Degree", ylab = "Probability of Degree")
+  hist_out <- hist(degree(g,mode="out"), breaks=seq(-0.5, by=1 , length.out=max(degree(g,mode="out"))+2), main="Out Degree Distribution", freq=FALSE,xlab = "Degree", ylab = "Probability of Degree")
+  
+  #part b
+  
+  dia <- diameter(g, directed = TRUE, unconnected = TRUE, weights = NULL)
+  print(sprintf("Diameter is: %d",dia))
+  
+  #part c
+  wc <- cluster_walktrap(g)
+  modularity(wc)
+  print(sprintf("Modularity is: %f",modularity(wc)))
+  #plot(wc, g)
+  #g_com <- infomap.community(g)
+  mod[i]=modularity(wc)
+}
+gg <- forest.fire.game (200, fw.prob=0.37, bw.factor=0.32/0.37, ambs=1, TRUE)
+wc <- cluster_walktrap(gg)
+plot(wc, gg,vertex.size=5,vertex.label=NA, layout=layout.fruchterman.reingold(gg),edge.arrow.size=0.2)
 
-g_in <- degree_distribution(g,mode="in")
-g_out <- degree_distribution(g,mode="out")
-
-# hist_in <- hist(degree(g,mode="in"), breaks=seq(-0.5, by=1 , length.out=max(degree(g,mode="in"))+2), main="In Degree Distribution")
-# dev.new()
-# hist_out <- hist(degree(g,mode="out"), breaks=seq(-0.5, by=1 , length.out=max(degree(g,mode="out"))+2), main="Out Degree Distribution")
-# dev.new()
-
-plot(1:length(g_in), g_in, "h", main="In Degree Distribution", xlab = "Degree", ylab = "Probability of Degree")
-dev.new()
-plot(1:length(g_out), g_out, "h", main="Out Degree Distribution", xlab = "Degree", ylab = "Probability of Degree")
-
-#part b
-
-dia <- diameter(g, directed = TRUE, unconnected = TRUE, weights = NULL)
-print(sprintf("Diameter is: %d",dia))
-
-#part c
-g_com <- infomap.community(g)
-print(sprintf("Modularity is: %f",max(g_com$modularity)))
