f772e99fc96cad4b99ec3f8926ea4e21477bbe39
Sonu Kumar Mishra <mishrasonu1993@gmail.com>
2016-04-21 21:10:02 -0700

    multiple rounds

--- Homework2/findDistanceAggregates.R
+++ Homework2/findDistanceAggregates.R
@@ -1,45 +1,49 @@
 library(igraph)
 library(netrw)
 
-findDistanceAggregates <- function(graph, nodes, flag, tlim)  # pass tlim as an argument
+findDistanceAggregates <- function(graph, nodes, flag, tlim, nround)  # pass tlim as an argument
 {
   
   avg <- rep(NA, tlim)
   stdev <- rep(NA, tlim)
   variance <- rep(NA, tlim)
-  
-  rw <- netrw(graph, walker.num=nodes, start.node=1:vcount(graph), damping=1, T=tlim, output.walk.path=TRUE)
-  
+    
   for(t in (1:tlim)) {
-    sd <- rep(NA,nodes)
-    for(j in (1:nodes)) {
-      sp <- get.shortest.paths(graph, from=rw$walk.path[1,j], to=rw$walk.path[t,j])
-      sd[j] <- length(sp$vpath[[1]])-1
+    sdr <- array(NA,c(nround, nodes)) 
+    for (r in (1:nround)) {
+      rw <- netrw(graph, walker.num=nodes, start.node=1:vcount(graph), damping=1, T=t, output.walk.path=TRUE)
+      for(j in (1:nodes)) {
+        sp <- get.shortest.paths(graph, from=rw$walk.path[1,j], to=rw$walk.path[t,j])
+        sdr[r,j] <- length(sp$vpath[[1]])-1
+      }
     }
-    avg[t] = mean(sd)
-    stdev[t] = sd(sd)
-    variance[t] = var(sd)
+    avg[t] = mean(sdr)
+    stdev[t] = sd(as.vector(sdr))
+    variance[t] = var(as.vector(sdr))
   }
   
-  print(avg)
+  # print(avg)
+  # print(stdev)
+  # print(variance)
   
   dev.new()
-  plot(1:tlim, avg, type="line", xlab="Number of Steps", ylab="Average")
+  plot(1:tlim, avg, type="l", xlab="Number of Steps", ylab="Average")
   dev.new()
-  plot(1:tlim, stdev, type="line", xlab="Number of Steps", ylab="Standard Deviation")
+  plot(1:tlim, stdev, type="l", xlab="Number of Steps", ylab="Standard Deviation")
   dev.new()
-  plot(1:tlim, variance, type="line", xlab="Number of Steps", ylab="Variance")
+  plot(1:tlim, variance, type="l", xlab="Number of Steps", ylab="Variance")
   
   #1 e
   if(flag==1) {
     dev.new()
-    hist(degree(graph), freq=FALSE, xlab = "Degree", ylab = "Probability of Degree")
+    hist(degree(graph), breaks=seq(0, by=1, length.out=25), freq=FALSE, xlab = "Degree", ylab = "Probability of Degree")
+    rw <- netrw(graph, walker.num=nodes, start.node=1:vcount(graph), damping=1, T=tlim, output.walk.path=TRUE)
     degeesVector <- rep(NA,nodes)
     for(i in (1:nodes)) {
       # print(i)
       degeesVector[i] = degree(graph,rw$walk.path[tlim,i])
     }
     dev.new()
-    hist(degeesVector, xlab="Degree", freq= FALSE, ylab="Probability of Degree")
+    hist(degeesVector, breaks=seq(0, by=1, length.out=25), xlab="Degree", freq= FALSE, ylab="Probability of Degree")
   }
 }
