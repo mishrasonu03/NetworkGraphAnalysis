0a099731089454068aaad75d92fda03196ce9e75
Sonu Kumar Mishra <mishrasonu1993@users.noreply.github.com>
2016-04-21 21:16:51 -0700

    Minor modification in function call

--- Homework2/q2.R
+++ Homework2/q2.R
@@ -7,14 +7,14 @@ g_degree=degree(g_a)
 hist(g_degree, freq=FALSE, breaks=seq(0, by=1, length.out=max(g_degree)+10), xlab = "Degree", ylab = "Probability of Degree")
 
 ## b
-findDistanceAggregates(g_a,1000,1,1000)
+findDistanceAggregates(g_a,1000,1,1000,1)
 
 ## d
 g_d_1 = barabasi.game(100,directed=FALSE)
-findDistanceAggregates(g_d_1,100,0,1000)
+findDistanceAggregates(g_d_1,100,0,1000,1)
 
 g_d_2 = barabasi.game(10000,directed=FALSE)
-findDistanceAggregates(g_d_2,10000,0,1000)
+findDistanceAggregates(g_d_2,10000,0,1000,1)
 
 
 #print Diameters for q1
