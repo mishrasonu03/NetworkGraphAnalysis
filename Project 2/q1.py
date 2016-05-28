7a96bdfe298fd378a0b41dc8666dd356bbb5b0e2
Yi Ding <lavenderdingyi@gmail.com>
2016-05-27 20:46:31 -0700

    Add files via upload

---
+++ Project 2/q1.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import re
+
+os.chdir(os.path.abspath(os.path.dirname(sys.argv[0])))
+
+#q1
+f_actor = open('actor_movies.txt','r')
+f_actress = open('actress_movies.txt','r')
+out = open('actor_actress.txt','w+')
+actor = f_actor.readlines()
+actress = f_actress.readlines()
+
+actor_actress = []
+for line in actor:
+    line = re.sub(r'\(+uncredited+\)','',line)
+    line = re.sub(r'\(+uncredited voice+\)','',line)
+    line = re.sub(r'\(+voice+\)','',line)
+    line = re.sub(r'\(+VG+\)','',line)
+    line = re.sub(r'\{\{+SUSPENDED+\}\}','',line)
+    line = re.sub(r'^\s+|\s+$','',line)
+    line = re.sub(r'\(+rumored+\)','',line)
+    line = re.sub(r'\(+credit only+\)','',line)
+    line = re.sub(r'\(+as .*','',line)
+    
+    tmp = re.split(r'\t+',line)
+    if len(tmp)>=6:
+        actor_actress.append(tmp)
+        tmp = '\t\t'.join(tmp)
+        out.write(tmp)
+
+for line in actress:
+    line = re.sub(r'\(+uncredited+\)','',line)
+    line = re.sub(r'\(+uncredited voice+\)','',line)
+    line = re.sub(r'\(+voice+\)','',line)
+    line = re.sub(r'\(+VG+\)','',line)
+    line = re.sub(r'\{\{+SUSPENDED+\}\}','',line)
+    line = re.sub(r'^\s+|\s+$','',line)
+    line = re.sub(r'\(+rumored+\)','',line)
+    line = re.sub(r'\(+credit only+\)','',line)
+    line = re.sub(r'\(+as .*','',line)
+    
+    tmp = re.split(r'\t+',line)
+    if len(tmp)>=6:
+        actor_actress.append(tmp)
+        tmp = '\t\t'.join(tmp)
+        out.write(tmp)
+
+f_actor.close()
+f_actress.close()
+out.close()
+
+
+
+
---
+++ Project 2/q2.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import re
+import numpy as np
+
+os.chdir(os.path.abspath(os.path.dirname(sys.argv[0])))
+
+# read data
+fa = open('actor_actress.txt','r')
+actor_actress_lines = fa.readlines()
+
+actor_actress = []
+
+for line in actor_actress_lines:
+    tmp = re.split(r'\t+',line)
+    actor_actress.append(tmp)
+
+fa.close()
+
+# process
+len_all = len(actor_actress)
+Movie2Actor_h = {}
+Edge_h = {}
+MNum2Name = {}
+ANum2Name = {}
+num_movie_act = []
+Act_ID = 0
+Movie_ID = 0
+
+print "hashing process"
+for i in range(len_all):
+    tmpline = actor_actress[i]
+    len_tmp = len(tmpline)
+    num_movie_act.append(len_tmp-1)
+    tmp_act = tmpline[0]
+    ANum2Name[Act_ID] = tmp_act
+
+    for j in range(1,len_tmp):
+        if tmpline[j] not in Movie2Actor_h:
+            Movie2Actor_h[tmpline[j]] = [Act_ID]
+            MNum2Name[Movie_ID] = tmpline[j]
+            Movie_ID = Movie_ID + 1
+        else:
+            Movie2Actor_h[tmpline[j]].append(Act_ID)
+    Act_ID = Act_ID + 1
+
+print "creating edgelist"
+for key in Movie2Actor_h:
+    if (len(Movie2Actor_h[key])<=1):
+        continue
+    for j in range(len(Movie2Actor_h[key])-1):
+        for k in range(j+1,len(Movie2Actor_h[key])):
+            tmp_edge = (Movie2Actor_h[key][j],Movie2Actor_h[key][k])
+            if tmp_edge not in Edge_h:
+                Edge_h[tmp_edge] = 1
+            else:
+                Edge_h[tmp_edge] = Edge_h[tmp_edge] + 1
+
+print "creating graph matrix"
+edgelist = np.zeros(shape=(len(Edge_h)*2,3))
+index = 0
+for ekey in Edge_h:
+    if index%10000==0:
+        print index
+    ENum = Edge_h[ekey]
+    vertex_1 = ekey[0]
+    vertex_2 = ekey[1]
+
+    weight_1 = ENum / num_movie_act[vertex_1]
+    weight_2 = ENum / num_movie_act[vertex_2]
+    edgelist[index][0] = vertex_1
+    edgelist[index][1] = vertex_2
+    edgelist[index][2] = weight_1
+    index = index + 1
+    edgelist[index][0] = vertex_2
+    edgelist[index][1] = vertex_1
+    edgelist[index][2] = weight_2
+    index = index + 1
+
