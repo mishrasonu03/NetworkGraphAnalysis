ff4b9bac5a66073c7de69c88db3b9f42edd9091f
Stonre <leiding007@gmail.com>
2016-06-02 15:10:42 +0800

    Add files via upload

--- Project 2/q1.py
+++ Project 2/q1.py
@@ -7,53 +7,57 @@ os.chdir(os.path.abspath(os.path.dirname(sys.argv[0])))
 #q1
 f_actor = open('actor_movies.txt','r')
 f_actress = open('actress_movies.txt','r')
-out = open('actor_actress.txt','w+')
+out = open("actor_actress.txt",'w+')
 actor = f_actor.readlines()
 actress = f_actress.readlines()
 
+count = 0
 actor_actress = []
-for line in actor:
-    line = re.sub(r'\(+uncredited+\)','',line)
-    line = re.sub(r'\(+uncredited voice+\)','',line)
-    line = re.sub(r'\(+voice+\)','',line)
-    line = re.sub(r'\(+VG+\)','',line)
-    line = re.sub(r'\{\{+SUSPENDED+\}\}','',line)
-    line = re.sub(r'^\s+|\s+$','',line)
-    line = re.sub(r'\(+rumored+\)','',line)
-    line = re.sub(r'\(+credit only+\)','',line)
-    line = re.sub(r'\(+as .*','',line)
-    
-    tmp = re.split(r'\t+',line)
-    if len(tmp)>=6:
-        for j in range(len(tmp)):
-            tmp[j] = re.sub(r'^\s+|\s+$','',tmp[j])
-        actor_actress.append(tmp)
-        tmp = '\t\t'.join(tmp)
-        out.write(tmp)
-        out.write('\n')
+with open("actor_actress.txt",'w+') as out:
+    for line in actor:
+        line = re.sub(r'\(+uncredited+\)','',line)
+        line = re.sub(r'\(+uncredited voice+\)','',line)
+        line = re.sub(r'\(+voice+\)','',line)
+        line = re.sub(r'\(+VG+\)','',line)
+        line = re.sub(r'\{\{+SUSPENDED+\}\}','',line)
+        line = re.sub(r'^\s+|\s+$','',line)
+        line = re.sub(r'\(+rumored+\)','',line)
+        line = re.sub(r'\(+credit only+\)','',line)
+        line = re.sub(r' \(+as .*?\)','',line)
 
-for line in actress:
-    line = re.sub(r'\(+uncredited+\)','',line)
-    line = re.sub(r'\(+uncredited voice+\)','',line)
-    line = re.sub(r'\(+voice+\)','',line)
-    line = re.sub(r'\(+VG+\)','',line)
-    line = re.sub(r'\{\{+SUSPENDED+\}\}','',line)
-    line = re.sub(r'^\s+|\s+$','',line)
-    line = re.sub(r'\(+rumored+\)','',line)
-    line = re.sub(r'\(+credit only+\)','',line)
-    line = re.sub(r'\(+as .*','',line)
-    
-    tmp = re.split(r'\t+',line)
-    if len(tmp)>=6:
-        for j in range(len(tmp)):
-            tmp[j] = re.sub(r'^\s+|\s+$','',tmp[j])
-        actor_actress.append(tmp)
-        tmp = '\t\t'.join(tmp)
-        out.write(tmp)
-        out.write('\n')
+        tmp = re.split(r'[\t]+',line)
+        if len(tmp)>=6:
+            count = count + 1
+            for j in range(len(tmp)):
+                tmp[j] = re.sub(r'^\s+|\s+$','',tmp[j])
+            actor_actress.append(tmp)
+            tmp = '\t\t'.join(tmp)
+            out.write(tmp)
+            out.write('\n')
 
-f_actor.close()
-f_actress.close()
+    for line in actress:
+        line = re.sub(r'\(+uncredited+\)','',line)
+        line = re.sub(r'\(+uncredited voice+\)','',line)
+        line = re.sub(r'\(+voice+\)','',line)
+        line = re.sub(r'\(+VG+\)','',line)
+        line = re.sub(r'\{\{+SUSPENDED+\}\}','',line)
+        line = re.sub(r'^\s+|\s+$','',line)
+        line = re.sub(r'\(+rumored+\)','',line)
+        line = re.sub(r'\(+credit only+\)','',line)
+        line = re.sub(r' \(+as .*?\)','',line)
+
+        tmp = re.split(r'[\t]+',line)
+        if len(tmp)>=6:
+            count = count + 1
+            for j in range(len(tmp)):
+                tmp[j] = re.sub(r'^\s+|\s+$','',tmp[j])
+            actor_actress.append(tmp)
+            tmp = '\t\t'.join(tmp)
+            out.write(tmp)
+            out.write('\n')
+
+    f_actor.close()
+    f_actress.close()
 out.close()
 
 
